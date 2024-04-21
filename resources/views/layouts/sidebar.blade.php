da30c2b21a6be421e8624ed5a278e3c401c26ab4
jison-jpeg <2101101090@student.buksu.edu.ph>
2024-04-22 00:12:11 +0800

    Update role middleware in web.php to include 'support' role

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,84 +9,86 @@
 
     <ul class="sidebar-nav" id="sidebar-nav">
         <li class="nav-heading">HOME</li>
-        @if(Auth::user()->role !== 'instructor' && Auth::user()->role !== 'dean' && Auth::user()->role !== 'chairperson')
+        @if (Auth::user()->role !== 'instructor' && Auth::user()->role !== 'dean' && Auth::user()->role !== 'chairperson')
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('dashboard*') ? '' : 'collapsed' }}" href="{{ url('/dashboard') }}">
+                    <i class="bi bi-grid"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('dashboard*') ? '' : 'collapsed' }}" href="{{ url('/dashboard') }}">
-                <i class="bi bi-grid"></i>
-                <span>Dashboard</span>
-            </a>
-        </li>
+            <li class="nav-heading">MENU</li>
 
-        <li class="nav-heading">MENU</li>
-        
-        {{-- Hide if user is instructoror dean or chairperson --}}
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('laboratories*') ? '' : 'collapsed' }}"
-                href="{{ url('/laboratories') }}">
-                <i class="bi bi-view-stacked"></i>
-                <span>Room Management</span>
-            </a>
-        </li>
+            {{-- Hide if user is instructoror dean or chairperson --}}
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('laboratories*') ? '' : 'collapsed' }}"
+                    href="{{ url('/laboratories') }}">
+                    <i class="bi bi-view-stacked"></i>
+                    <span>Room Management</span>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('users*') ? '' : 'collapsed' }}" href="{{ url('/users') }}">
-                <i class="bi bi-people"></i>
-                <span>User Management</span>
-            </a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('users*') ? '' : 'collapsed' }}" href="{{ url('/users') }}">
+                    <i class="bi bi-people"></i>
+                    <span>User Management</span>
+                </a>
+            </li>
         @endif
 
-        {{-- @if(Auth::user()->role !== 'admin') --}}
+        @if (Auth::user()->role !== 'support')
         <li class="nav-item">
             <a class="nav-link {{ request()->is('attendance*') ? '' : 'collapsed' }}" href="{{ url('/attendance') }}">
                 <i class="bi bi-clipboard2-check"></i>
                 <span>Attendance</span>
             </a>
         </li>
-        {{-- @endif --}}
-
-
-        @if(Auth::user()->role !== 'instructor' && Auth::user()->role !== 'dean' && Auth::user()->role !== 'chairperson')
-        <li class="nav-heading">INSTITUTION</li>
-
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('colleges*') ? '' : 'collapsed' }}" href="{{ url('/colleges') }}">
-                <i class="bi bi-bank"></i>
-                <span>College Management</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('faculties*') ? '' : 'collapsed' }}"
-                href="{{ url('/faculties') }}">
-                <i class="bi bi-mortarboard"></i>
-                <span>Faculty Management</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('students*') ? '' : 'collapsed' }}" href="{{ url('/students') }}">
-                <i class="bi bi-person-workspace"></i>
-                <span>Student Management</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('subjects*') ? '' : 'collapsed' }}" href="{{ url('/subjects') }}">
-                <i class="bi bi-book"></i>
-                <span>Subject Management</span>
-            </a>
-        </li>
         @endif
 
+
+
+        @if (Auth::user()->role !== 'instructor' && Auth::user()->role !== 'dean' && Auth::user()->role !== 'chairperson')
+            <li class="nav-heading">INSTITUTION</li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('colleges*') ? '' : 'collapsed' }}" href="{{ url('/colleges') }}">
+                    <i class="bi bi-bank"></i>
+                    <span>College Management</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('faculties*') ? '' : 'collapsed' }}"
+                    href="{{ url('/faculties') }}">
+                    <i class="bi bi-mortarboard"></i>
+                    <span>Faculty Management</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('students*') ? '' : 'collapsed' }}" href="{{ url('/students') }}">
+                    <i class="bi bi-person-workspace"></i>
+                    <span>Student Management</span>
+                </a>
+            </li>
+
+            @if (Auth::user()->role !== 'support')
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('subjects*') ? '' : 'collapsed' }}" href="{{ url('/subjects') }}">
+                    <i class="bi bi-book"></i>
+                    <span>Subject Management</span>
+                </a>
+            </li>
+            @endif
+        @endif
+        @if (Auth::user()->role !== 'support')
         <li class="nav-item">
             <a class="nav-link {{ request()->is('schedules*') ? '' : 'collapsed' }}" href="{{ url('/schedules') }}">
                 <i class="bi bi-calendar2-week"></i>
                 <span>Class Schedule</span>
             </a>
         </li>
-
+        @endif
 
         <li class="nav-heading">USER</li>
         <li class="nav-item">
@@ -96,17 +98,17 @@
             </a>
         </li>
 
-        @if(Auth::user()->role !== 'instructor' && Auth::user()->role !== 'dean' && Auth::user()->role !== 'chairperson')
-        <li class="nav-heading">SETTINGS</li>
-        <li class="nav-item">
-            <a class="nav-link {{ request()->is('logs') ? '' : 'collapsed' }}" href="{{ url('/logs') }}">
-                <i class="bx bx-detail"></i>
-                <span>Logs</span>
-            </a>
-        </li>
+        @if (Auth::user()->role !== 'instructor' && Auth::user()->role !== 'dean' && Auth::user()->role !== 'chairperson')
+            <li class="nav-heading">SETTINGS</li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('logs') ? '' : 'collapsed' }}" href="{{ url('/logs') }}">
+                    <i class="bx bx-detail"></i>
+                    <span>Logs</span>
+                </a>
+            </li>
         @endif
 
-        {{-- @if(Auth::user()->role !== 'admin')
+        {{-- @if (Auth::user()->role !== 'admin')
         <li class="nav-item">
             <a class="nav-link {{ request()->is('reports') ? '' : 'collapsed' }}" href="{{ url('/reports') }}">
                 <i class="bi bi-graph-up-arrow"></i>
--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::post('attendance/record-attendance', [AttendanceController::class, 'recordAttendance'])->name('record-attendance')->middleware('guest');
 });
 
-Route::group(['middleware' => ['auth', 'role:admin,support']], function () {
+Route::group(['middleware' => ['auth', 'role:admin']], function () {
     // Dashboard Routes
     Route::get('/dashboard', [Dashboard::class, 'viewDashboard']);
 
@@ -111,7 +111,7 @@ Route::group(['middleware' => ['auth', 'role:admin,support']], function () {
 });
 
 // Routes for Admin and Instructor
-Route::group(['middleware' => ['auth', 'role:admin,instructor,support']], function () {
+Route::group(['middleware' => ['auth', 'role:admin,instructor']], function () {
     // // Dashboard Routes
     // Route::get('/dashboard', [Dashboard::class, 'viewDashboard']);
 
@@ -165,4 +165,77 @@ Route::group(['middleware' => ['auth', 'role:admin,instructor,support']], functi
         Route::get('/attendance', [AttendanceReportController::class, 'index'])->name('attendance.tableReport');
         Route::get('/attendance/{sectionId}/{subjectId}', [AttendanceReportController::class, 'viewStudentAttendance'])->name('attendanceStudent.tableReport');
     });
+});
+
+// Routes for Support
+Route::group(['middleware' => ['auth', 'role:support']], function () {
+    // Dashboard Route
+    Route::get('/dashboard', [Dashboard::class, 'viewDashboard'])->name('dashboard');
+
+     // Laboratories Route
+    Route::group(['prefix' => 'laboratories'], function () {
+        Route::get('/', [LaboratoriesController::class, 'viewLaboratories'])->name('laboratories');
+        Route::post('/', [LaboratoriesController::class, 'laboratoriesPost'])->name('laboratories.post');
+        Route::put('/{id}', [LaboratoriesController::class, 'laboratoriesPut'])->name('laboratories.update');
+        Route::delete('/{id}', [LaboratoriesController::class, 'laboratoriesDelete'])->name('laboratories.delete');
+        Route::put('/laboratories/{id}/updateLockStatus', [LaboratoriesController::class, 'updateLockStatus'])->name('updateLockStatus');
+    });
+
+    // User Routes
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', [UsersController::class, 'viewUsers'])->name('users');
+        Route::post('/', [UsersController::class, 'usersPost'])->name('users.post');
+        Route::put('/{id}', [UsersController::class, 'usersPut'])->name('users.update');
+        Route::delete('/{id}', [UsersController::class, 'usersDelete'])->name('users.delete');
+        Route::get('/{id}', [UsersController::class, 'viewUserReports'])->name('user.report');
+    });
+
+    // Student Routes
+    Route::group(['prefix' => 'students'], function () {
+        Route::get('/', [StudentController::class, 'viewStudents'])->name('students');
+        Route::post('/', [StudentController::class, 'studentsPost'])->name('students.post');
+        Route::put('/{id}', [StudentController::class, 'studentsPut'])->name('students.put');
+        Route::delete('/{id}', [StudentController::class, 'studentsDelete'])->name('students.delete');
+        Route::get('/instructor/{id}', [StudentController::class, 'viewStudentsByInstructor'])->name('students.instructor');
+    });
+
+    // College Management Route
+    Route::group(['prefix' => 'colleges'], function () {
+        Route::get('/', [CollegeManagementController::class, 'viewColleges'])->name('colleges');
+        Route::post('/', [CollegeManagementController::class, 'createCollege'])->name('colleges.create');
+        Route::put('/{id}', [CollegeManagementController::class, 'collegePut'])->name('college.update');
+        Route::delete('/{id}', [CollegeManagementController::class, 'collegeDelete'])->name('college.delete');
+        // Department Management Route
+        Route::post('/departments', [CollegeManagementController::class, 'departmentsPost'])->name('departments.post');
+        Route::put('/departments/{id}', [CollegeManagementController::class, 'departmentPut'])->name('departments.update');
+        Route::delete('/departments/{id}', [CollegeManagementController::class, 'departmentDelete'])->name('departments.delete');
+        // Section Management Route
+        Route::post('/sections', [CollegeManagementController::class, 'sectionsPost'])->name('sections.create');
+        Route::put('/sections/{id}', [CollegeManagementController::class, 'sectionPut'])->name('sections.update');
+        Route::delete('/sections/{id}', [CollegeManagementController::class, 'sectionDelete'])->name('sections.delete');
+    });
+
+    // Faculty Routes
+    Route::group(['prefix' => 'faculties'], function () {
+        Route::get('/', [FacultyController::class, 'viewFaculties'])->name('faculties');
+        Route::post('/', [FacultyController::class, 'facultiesPost'])->name('faculties.post');
+        Route::put('/{id}', [FacultyController::class, 'facultiesPut'])->name('faculties.put');
+        Route::delete('/{id}', [FacultyController::class, 'facultiesDelete'])->name('faculties.delete');
+    });
+
+    // Logs Route
+    Route::group(['prefix' => 'logs'], function () {
+        Route::get('/', [LogsController::class, 'viewLogs'])->name('logs');
+        Route::get('/latest', [LogsController::class, 'latestLog']);
+        Route::get('/user/{userId}', [LogsController::class, 'logsByUser'])->name('logs.byUser');
+        Route::get('/laboratory/{id}', [LogsController::class, 'viewLogsByLaboratory'])->name('logs.byLaboratory');
+    });
+
+    // Profile Routes
+    Route::group(['prefix' => 'profile'], function () {
+        Route::get('/', [ProfileController::class, 'viewProfile'])->name('profile');
+        Route::put('/', [ProfileController::class, 'profilePut'])->name('profile.put');
+        Route::put('/password', [ProfileController::class, 'passwordPut'])->name('password.put');
+    });
+
 });
